bb8bc3e2ec26d23799d7095e2353c45484f20a2a
Mesbah-M13 <mesbah0010@gmail.com>
2025-10-25 17:27:33 +0600

    initial stage: navbar completed

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,89 @@
+"use client"
+
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { RiCloseLargeFill } from 'react-icons/ri'
+
+const Navbar = () => {
+
+  const MobileNav = [
+    { name: 'Features', href: '#' },
+    { name: 'Pricing', href: '#' },
+    { name: 'Security', href: '#' },
+    { name: 'Benefits', href: '#' },
+    { name: 'Contact Us', href: '/' },
+  ]
+
+  const [menuOpen, setMenuOpen] = useState(false)
+  const toggleMenu = () => setMenuOpen(!menuOpen)
+
+  return (
+    <nav>
+      <div className="w-11/12 md:max-w-9/10 mx-auto flex items-center justify-between">
+        <div className="flex items-center gap-0">
+          <Image
+            src="/assets/logo.svg"
+            alt="logo"
+            width={55}
+            height={0}
+            className="object-contain align-middle"
+          />
+          <h1 className="text-2xl md:text-4xl mb-2 font-semibold tracking-tight leading-none whitespace-nowrap">
+            Easy Pay
+          </h1>
+        </div>
+
+        <div className='w-7/12 '>
+          <div className='sm:flex items-center justify-between hidden '>
+            <ul className='flex gap-8  '>
+              <li>Features</li>
+              <li>Pricing</li>
+              <li>Scurity</li>
+              <li>Benefits</li>
+            </ul>
+
+            <div>
+              <Link href={'/'}> Contact Us</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className='md:hidden pl-4'>
+          {menuOpen ? <RiCloseLargeFill onClick={toggleMenu} />
+            : <GiHamburgerMenu onClick={toggleMenu} />
+          }
+        </div>
+
+      </div>
+
+      <div>
+        {
+          menuOpen &&
+          <ul className="flex flex-col items-center justify-center w-full space-y-2 md:hidden">
+            {
+              MobileNav.map((menu, index) => (
+                <li key={index} className='w-10/12 text-center cursor-pointer transition-all duration-200 rounded-xl px-4 py-1.5 ' >
+                  <Link
+                    href={menu.href}
+                    className={`block text-center cursor-pointer px-4 py-2 rounded-md transition-all duration-300 ease-in-out 
+                      ${menu.name === "Contact Us"
+                        ? "bg-gradient-to-r from-purple-600 to-sky-500 text-white hover:from-purple-600 hover:to-sky-600 text-xl"
+                        : "hover:bg-blue-500 hover:text-white"
+                      }`}
+                  >
+                    {menu.name}
+                  </Link>
+
+                </li>
+              ))
+            }
+          </ul>
+        }
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
