708059944f2f06e3b31cf3a7bc7a1da343f4d206
Mesbah-M13 <mesbah0010@gmail.com>
2025-10-29 11:00:33 +0600

    tweaks in herosection & price card

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -8,8 +8,8 @@ import PaymentReceived from "./PaymentReceived";
 
 const HeroSection = () => {
   return (
-    <div className="w-full flex md:w-10/12 md:max-w-10/12 mx-auto">
-      <div className=" flex flex-col ml-6 md:ml-4 mt-8 md:mt-28">
+    <div className="w-full flex md:w-11/12 md:max-w-11/12 mx-auto">
+      <div className=" flex flex-col mx-6 md:ml-22 mt-8 xl:mt-28">
         <div className="relative">
           <p className="text-gray-500 mb-2 font-semibold text-sm md:text-lg">EASY PAYMENT</p>
 
@@ -25,7 +25,7 @@ const HeroSection = () => {
             </span>{" "}
           </h1>
 
-          <p className="mt-6 text-[12px] text-gray-600 md:text-gray-500 tracking-wide">
+          <p className="mt-6 text-[12px] sm:text-[14px] text-gray-600 md:text-gray-500 tracking-wide">
             Experience the future of payments:fast,secure,and tailored
             <br className="hidden md:block" />
             for the next gerneration's convenience and trust.
@@ -37,10 +37,10 @@ const HeroSection = () => {
             alt="line"
             width={450}
             height={0}
-            className="hidden md:block object-contain absolute top-28 right-20 translate-x-10"
+            className="hidden md:block object-contain absolute top-28 xl:right-20 translate-x-10"
           />
         </div>
-        <div className="md:w-7/12 flex items-center justify-baseline gap-6 md:gap-0 md:justify-between mt-8 md:mt-14 text-gray-600">
+        <div className="md:w-[60%] xl:w-[52%] flex items-center justify-baseline gap-6 lg:gap-8  md:gap-0 md:justify-between mt-8 md:mt-14 lg:mt-4 xl:mt-14 text-gray-600">
           <div className="flex gap-1 md:gap-2 bg-black px-4 py-4 md:px-2 md:py-4 rounded-2xl items-center">
             <SiApple className="text-2xl md:text-4xl text-white" />
             <span className="text-white">
@@ -48,7 +48,22 @@ const HeroSection = () => {
               <h1 className=" text-sm md:text-md font-medium">App Store</h1>
             </span>
           </div>
-          <div className="flex gap-2 md:gap-4 bg-black px-3 py-4 md:px-4 md:py-4 rounded-2xl items-center">
+          <div className="flex gap-1 md:gap-2 bg-black px-4 py-4 md:px-3 lg:px-1 md:py-4 rounded-2xl items-center">
+            <Image
+              src="/assets/google-playstore.svg"
+              alt="playstore logo"
+              width={0}
+              height={0}
+              className="w-6 h-6 md:w-6 md:h-6"
+
+            />
+            <span className="text-white xl:ml-1.5 lg:w-[9vw] ">
+              <p className="text-[8px] md:text-xs">GET IT ON</p>
+              <h1 className=" text-sm md:text-md font-medium">Google Play</h1>
+            </span>
+          </div>
+
+          {/* <div className="flex gap-2 md:gap-4 bg-black px-3 py-4 md:px-4 md:py-4 rounded-2xl items-center">
             <Image
               src="/assets/google-playstore.svg"
               alt="playstore logo"
@@ -61,10 +76,10 @@ const HeroSection = () => {
               <p className="text-[8px] md:text-xs">GET IT ON</p>
               <h1 className="text-sm md:text-md font-normal md:font-medium">Google Play</h1>
             </span>
-          </div>
+          </div> */}
         </div>
 
-        <div className="flex flex-col md:flex-row md:items-center md:justify-baseline gap-2 md:gap-4 mt-6 md:mt-4">
+        <div className="flex flex-col md:flex-row md:items-center md:justify-baseline gap-2 md:gap-4 mt-6 md:mt-4 lg:mb-12">
           <div className="flex items-center gap-2 ">
             <PiCheckCircleBold
               className="text-gray-600 text-md md:text-xl"
@@ -83,12 +98,12 @@ const HeroSection = () => {
       </div>
 
 
-      <div className="md:w-5/12 relative h-[80vh]">
+      <div className="hidden lg:w-6/10 xl:w-5/12 lg:block lg:relative lg:h-[60vh] xl:h-[82vh]">
         <Image
           src="/assets/hero_man-Photoroom.png"
           alt="man hero img"
           fill
-          className="hidden md:block absolute left-8 object-cover object-[left_76%_top_50%] z-50"
+          className="hidden lg:block absolute left-10 object-cover object-[left_75%_top_50%] z-50"
         />
         <div className=" absolute top-100 right-25 md:top-60 md:-right-20 z-50 ">
           <UserCountBadge />
@@ -97,6 +112,9 @@ const HeroSection = () => {
           <PaymentReceived />
         </div>
       </div>
+
+
+
     </div>
   );
 };
--- src/components/sections/PricingCard.tsx
+++ src/components/sections/PricingCard.tsx
@@ -3,9 +3,9 @@ import { FaRegCheckCircle } from "react-icons/fa";
 
 const PricingCard = () => {
   return (
-    <div className="flex mt-12 flex-col md:flex-row justify-between items-center gap-8 ">
+    <div className="flex mt-12 flex-col md:flex-row justify-between items-center md:gap-4 xl:gap-8 ">
       {/* <!-- Card 1 --> */}
-      <div className="bg-white rounded-2xl shadow-lg w-full sm:w-80 md:w-96 h-[500px] p-14 border-2 border-gray-200 flex flex-col justify-between transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:-translate-y-2">
+      <div className="bg-white rounded-2xl shadow-lg w-full sm:w-80 md:w-84 xl:dw-96 h-[500px] p-14 border-2 border-gray-200 flex flex-col justify-between transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:-translate-y-2">
         <div>
           <h3 className="text-xl font-semibold text-gray-900">Free Plan</h3>
           <p className="text-gray-500 mt-1">Free for personal payments.</p>
@@ -44,7 +44,7 @@ const PricingCard = () => {
       </div>
 
       {/* <!-- Card 2 (Middle Highlight) --> */}
-      <div className="bg-black rounded-2xl shadow-lg w-full sm:w-80 md:w-96 h-[500px] p-11 flex flex-col justify-between transition-all duration-500 hover:scale-110 hover:shadow-[0_0_40px_rgba(0,0,0,0.5)] hover:-translate-y-3">
+      <div className="bg-black rounded-2xl shadow-lg w-full sm:w-80 md:w-84 xl:dw-96 h-[500px] p-11 flex flex-col justify-between transition-all duration-500 hover:scale-110 hover:shadow-[0_0_40px_rgba(0,0,0,0.5)] hover:-translate-y-3">
         <div>
           <h3 className="text-xl font-semibold text-white">Advanced</h3>
           <p className="text-gray-400 mt-1">
@@ -95,7 +95,7 @@ const PricingCard = () => {
       </div>
 
       {/* <!-- Card 3 --> */}
-      <div className="bg-white rounded-2xl shadow-lg w-full sm:w-80 md:w-96 h-[500px] p-11 flex flex-col border-2 border-gray-200 justify-between transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:-translate-y-2">
+      <div className="bg-white rounded-2xl shadow-lg w-full sm:w-80 md:w-84 xl:dw-96 h-[500px] p-11 flex flex-col border-2 border-gray-200 justify-between transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:-translate-y-2">
         <div>
           <h3 className="text-xl font-semibold text-gray-900">Business</h3>
           <p className="text-gray-500 mt-1">Premium business transactions</p>
@@ -107,25 +107,25 @@ const PricingCard = () => {
           <hr className="m-8 h-[2px] border-0 bg-gradient-to-r from-white via-gray-300 to-white" />
           <ul className="mt-6 space-y-3 text-gray-600">
             <li className="flex items-center gap-2">
-              <span className="text-sky-400">
+              <span className="text-blue-600">
                 <FaRegCheckCircle className="bg-gray-300 rounded-full" />
               </span>{" "}
               Up to 1000 transections per month
             </li>
             <li className="flex items-center gap-2">
-              <span className="text-sky-400">
+              <span className="text-blue-600">
                 <FaRegCheckCircle className="bg-gray-300 rounded-full" />
               </span>{" "}
               Advanced fraud protection
             </li>
             <li className="flex items-center gap-2">
-              <span className="text-sky-400">
+              <span className="text-blue-600">
                 <FaRegCheckCircle className="bg-gray-300 rounded-full" />
               </span>{" "}
               Detailed transaction reports
             </li>
             <li className="flex items-center gap-2">
-              <span className="text-sky-400">
+              <span className="text-blue-600">
                 <FaRegCheckCircle className="bg-gray-300 rounded-full" />
               </span>{" "}
               Priority email & chat support
